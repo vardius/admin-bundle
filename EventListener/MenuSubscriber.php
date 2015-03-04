8c52e99e4470571d54e2bfc9bd5832b1b5a41858
Rafał Lorenz <vardius@gmail.com>
2015-03-04 14:55:19 +0100

    set dashboard as first menu item

--- EventListener/MenuSubscriber.php
+++ EventListener/MenuSubscriber.php
@@ -7,6 +7,7 @@
 
 namespace Vardius\Bundle\AdminBundle\EventListener;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Vardius\Bundle\MenuBundle\Event\MenuEvent;
 use Vardius\Bundle\MenuBundle\Event\MenuEvents;
@@ -34,7 +35,15 @@ class MenuSubscriber implements EventSubscriberInterface
         $menu = $event->getMenu();
         if ($menu->getName() === 'admin_menu') {
 
+            $children = $menu->getChildren();
+
+            $menu->setChildren(new ArrayCollection());
             $menu->addChild(new MenuItem('menu.dashboard', 'dashboard', [], 'fa-tachometer'));
+
+            foreach ($children as $child) {
+                $menu->addChild($child);
+            }
+
         }
     }
 }
