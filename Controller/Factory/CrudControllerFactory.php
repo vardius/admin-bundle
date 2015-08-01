cc2f617862486b35113b05658d7308ac83d9a51d
Rafał Lorenz <vardius@gmail.com>
2015-08-01 14:13:11 +0200

    updated crud controller factory

--- Controller/Factory/CrudControllerFactory.php
+++ Controller/Factory/CrudControllerFactory.php
@@ -8,6 +8,7 @@
 namespace Vardius\Bundle\AdminBundle\Controller\Factory;
 
 use Symfony\Component\Form\AbstractType;
+use Vardius\Bundle\CrudBundle\Manager\CrudManagerInterface;
 use Vardius\Bundle\ListBundle\ListView\Provider\ListViewProviderInterface;
 
 /**
@@ -17,9 +18,9 @@ use Vardius\Bundle\ListBundle\ListView\Provider\ListViewProviderInterface;
  */
 class CrudControllerFactory extends \Vardius\Bundle\CrudBundle\Controller\Factory\CrudControllerFactory
 {
-    public function get($entityName, $routePrefix = '', ListViewProviderInterface $listViewProvider = null, AbstractType $formType = null, $view = null, array $actions = [])
+    public function get($entityName, $routePrefix = '', ListViewProviderInterface $listViewProvider = null, AbstractType $formType = null, CrudManagerInterface $crudManager = null, $view = null, array $actions = [])
     {
-        return parent::get($entityName, '/admin' . $routePrefix, $listViewProvider, $formType, $view, $actions);
+        return parent::get($entityName, '/admin' . $routePrefix, $listViewProvider, $formType, $crudManager, $view, $actions);
     }
 
 }
