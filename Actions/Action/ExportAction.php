2698fe19d349c5c880232bf265a3f106908b2f1a
Rafał Lorenz <vardius@gmail.com>
2015-08-02 16:08:07 +0200

    export action, uses new version of vardius crud bundle

---
+++ Actions/Action/ExportAction.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is part of the vardius/admin-bundle package.
+ *
+ * (c) Rafał Lorenz <vardius@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Vardius\Bundle\AdminBundle\Actions\Action;
+
+/**
+ * ExportAction
+ *
+ * @author Rafał Lorenz <vardius@gmail.com>
+ */
+class ExportAction extends \Vardius\Bundle\CrudBundle\Actions\Action\ExportAction
+{
+    protected static $TEMPLATE_DIR = 'VardiusAdminBundle:Actions:';
+}
